43dd74b5fe38feb0c7ed271fe1e1fb53b6a588dd
Harshit_Shakya <106598525+Harshit4712@users.noreply.github.com>
2024-03-18 15:05:10 +0530

    Add files via upload

---
+++ Digital Input with a Pushbutton/digital_input_with_a_pushbutton1.ino
@@ -0,0 +1,28 @@
+// Constants
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// Variables
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // Initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // Initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop(){
+  // Read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // Check if the pushbutton is pressed.
+  // If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {     
+    // Turn LED on:    
+    digitalWrite(ledPin, HIGH);  
+  } else {
+    // Turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
+}
